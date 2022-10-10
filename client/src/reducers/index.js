1eb064f8066d067edc6a18e1e0835862531217c4
Nabeel Muhammed <43858092+nabeelkv@users.noreply.github.com>
2022-10-10 23:43:38 +0530

    Delete client directory

--- client/src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-
-import postReducers from './postReducers';
-import authReducers from './authReducers';
-
-export default combineReducers({ postReducers, authReducers });
